0b9d20a99ca978ae60deffc9aefe600fe250f8b9
DESKTOP-B3EGD8V\HANULPARK <hanulparkk@gmail.com>
2020-08-17 23:13:10 +0900

    Programmers > Level2 > 더 맵게

---
+++ Programmers/Level2/더 맵게.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        int K = 7;
+
+        int answer = solution.solution(scoville, K);
+
+        System.out.println(answer);
+
+    }
+}
+
+//First Solution: PriorityQueue
+class Solution {
+    public int solution(int[] scoville, int K) {
+        if (scoville == null || scoville.length == 0) {
+            return -1;
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int val : scoville) {
+            pq.offer(val);
+        }
+
+        int answer = 0;
+        
+        while (pq.peek() < K) {
+            if (pq.size() == 1) {
+                return -1;
+            }
+
+            pq.offer(pq.poll() + (pq.poll() * 2));
+            answer++;
+        }
+
+        return answer;
+    }
+}
