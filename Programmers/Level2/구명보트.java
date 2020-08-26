c33a743b466bb352b4e3b6df1358251e132e7c0d
DESKTOP-B3EGD8V\HANULPARK <hanulparkk@gmail.com>
2020-08-26 22:59:26 +0900

    Programmers > Level2 > 구명보트

---
+++ Programmers/Level2/구명보트.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int[] people = {70, 50, 80, 50};
+        int limit = 100;
+
+        int answer = solution.solution(people, limit);
+
+        System.out.println(answer);
+
+    }
+}
+
+//First Solution
+class Solution {
+    public int solution(int[] people, int limit) {
+        int boat = 0;
+
+        Arrays.sort(people);
+
+        int left = 0;
+        int right = people.length - 1;
+
+        while (left < right) {
+            int weight = people[left] + people[right];
+
+            if (weight <= limit) {
+                left++;
+                right--;
+            } else {
+                right--;
+            }
+
+            boat++;
+        }
+
+        if(left == right)
+            boat++;
+
+        return boat;
+    }
+}
