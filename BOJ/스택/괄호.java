f2d3fa684e6a25cd434fe2318b7bb08dc59cc7a0
DESKTOP-B3EGD8V\HANULPARK <hanulparkk@gmail.com>
2020-11-27 21:45:39 +0900

    BOJ > 스택 > 괄호 - 스택의 타입 매개변수 선언

--- BOJ/스택/괄호.java
+++ BOJ/스택/괄호.java
@@ -2,13 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static Stack stack;
+    static Stack<Character> stack;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
-        stack = new Stack();
+        stack = new Stack<>();
 
         for (int i = 0; i < T; i++) {
             String PS = br.readLine();
